d3f50b76504444db70afb9e120cd3fe9f362116e
AntonioQk <86135476+AntonioQk@users.noreply.github.com>
2022-05-31 18:46:29 -0500

    Add files via upload

---
+++ js/filtrar_cliente_ingreso.js
@@ -0,0 +1,21 @@
+const co_c_i = document.getElementById('co_ingresar_c');
+const ce_c_i = document.getElementById('ce_ingresar_c');
+const cg_c_i = document.getElementById('cg_ingresar_c');
+const d_c_i = document.getElementById('d_ingresar_c');
+
+//------------------------------------------------
+co_c_i.addEventListener('click', () => {
+  window.location.href = "/ingreso/co";
+})
+
+ce_c_i.addEventListener('click', () => {
+  window.location.href = "/ingreso/ce";
+})
+
+cg_c_i.addEventListener('click', () => {
+  window.location.href = "/ingreso/cg";
+})
+
+d_c_i.addEventListener('click', () => {
+  window.location.href = "/ingreso";
+})
